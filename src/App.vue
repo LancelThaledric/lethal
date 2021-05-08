a5127302517489658154e74ba501263246dba0bd
thaledric <thaledric@gmail.com>
2021-05-08 23:16:09 +0200

    feat: header

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,48 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <Header/>
   <router-view/>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+* {
+  box-sizing: border-box;
 }
 
-#nav {
-  padding: 30px;
+html {
+  font-size: 16px; // rem definition
+}
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+body {
+  margin: 0;
+}
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+#app { /* #app tag is include in @/public/index.html */
+  font-family: 'Montserrat', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #111;
+
+  display: flex; flex-direction: column;
+}
+
+a {
+  color: #c02;
+
+  &:hover {
+    color: #e00;
   }
 }
 </style>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component';
+
+import Header from '@/components/Header.vue';
+
+@Options({
+  components: {
+    Header
+  }
+})
+export default class HelloWorld extends Vue {}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,59 @@
+<template>
+  <header class="lt-header">
+    <div class="lt-header-blur-0">
+      <div class="lt-header-inner">
+        <router-link to="/" class="lt-logo">Le Thal'</router-link>
+        <ul class="lt-nav"></ul>
+      </div>
+    </div>
+  </header>
+  <div class="lt-header-spacer" />
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({})
+export default class LTHeader extends Vue {}
+</script>
+
+<style scoped lang="scss">
+@import "@/ui/gradient.scss";
+
+.lt-header {
+  position: fixed;
+  width: 100%;
+  background: rgba(#fff, 0.95);
+
+  &::before {
+    content: "";
+    position: absolute;
+    top: 100%;
+    left: 0;
+    width: 100%;
+    height: 6rem;
+    @include quad-easing-gradient(to top, rgba(#fff, 0.95));
+  }
+}
+.lt-header-inner {
+  display: flex;
+
+  margin: 3rem auto 1rem;
+  width: 80%;
+}
+.lt-header-blur-0 {
+  backdrop-filter: blur(10px);
+	display: flex;
+}
+
+.lt-logo {
+  font-size: 3rem;
+  font-family: "Poiret One", cursive;
+  color: inherit;
+  text-decoration: inherit;
+}
+
+.lt-header-spacer {
+  height: 15rem;
+}
+</style>
